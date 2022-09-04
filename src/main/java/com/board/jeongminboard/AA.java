008647a1a2aebc26e97b9f632961c0b24c767ca8
Lim <dlawjdals0218@gmail.com>
2022-09-04 20:46:14 +0900

    dd

--- src/main/java/com/board/jeongminboard/AA.java
+++ src/main/java/com/board/jeongminboard/AA.java
@@ -3,5 +3,6 @@ package com.board.jeongminboard;
 public class AA {
     public static void main(String[] args) {
         System.out.println("zzzzz");
+        System.out.println("zzzzz");System.out.println("zzzzz");System.out.println("zzzzz");
     }
 }
